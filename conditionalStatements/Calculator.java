171123f8f506f799f1bf93772f0287a7d9939a29
devvijay17 <72187608+devvijay17@users.noreply.github.com>
2020-10-01 12:27:57 +0530

    Update Calculator.java

--- conditionalStatements/Calculator.java
+++ conditionalStatements/Calculator.java
@@ -9,9 +9,11 @@ public class Calculator {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Enter first number");
+		
 		int a = sc.nextInt();
 		
 		System.out.println("Enter second number");
+		
 		int b = sc.nextInt();
 		
 		System.out.println("Enter operation");
